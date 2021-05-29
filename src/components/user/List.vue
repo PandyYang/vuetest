3f279b884fb48e6ae972e7e05dcff0b094c36dad
PandyYang <yangpandy@gmail.com>
2021-05-29 21:32:20 +0800

    删除增加气泡弹出框

--- src/components/user/List.vue
+++ src/components/user/List.vue
@@ -37,14 +37,55 @@
           <el-button
             size="mini"
             @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          <el-popconfirm
+            confirm-button-text="好的"
+            cancel-button-text="不用了"
+            icon="el-icon-info"
+            icon-color="red"
+            title="这是一段内容确定删除吗？"
+            @confirm="handleDelete(scope.$index, scope.row)"
+          >
+            <el-button
+              size="mini"
+              type="danger"
+              slot="reference">删除</el-button>
+          </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
-    <el-button style="margin: 10px" type="success" size="mini">添加</el-button>
+    <el-button style="margin: 10px 0px" @click="show = !show" type="success" size="mini">添加</el-button>
+    <el-collapse-transition name="el-zoom-in-center">
+      <div v-show="show" class="transition-box">
+        <el-form ref="form" :model="form" label-suffix=":" label-width="80px">
+          <el-form-item label="姓名">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="生日">
+            <el-col>
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="form.bir"
+                value-format="yyyy-MM-dd HH:mm:ss"
+                style="width: 100%;"></el-date-picker>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio-group v-model="form.sex">
+              <el-radio label="男"></el-radio>
+              <el-radio label="女"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="信息地址">
+            <el-input type="textarea" v-model="form.address"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">创建用户</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-collapse-transition>
   </div>
 </template>
 
@@ -53,43 +94,19 @@ export default {
   name: 'List',
   data () {
     return {
-      tableData: [{
-        id: 1,
-        bir: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        sex: '男'
-      },
-      {
-        id: 2,
-        bir: '2016-05-02',
-        name: '杨二毛',
-        address: '上海市普陀区金沙江路 1518 弄',
-        sex: '男'
-      },
-      {
-        id: 3,
-        bir: '2016-05-02',
-        name: '李云云',
-        address: '上海市普陀区金沙江路 1518 弄',
-        sex: '男'
-      },
-      {
-        id: 4,
-        bir: '2016-05-02',
-        name: '逆水寒',
-        address: '上海市普陀区金沙江路 1518 弄',
-        sex: '男'
-      },
-      {
-        id: 5,
-        bir: '2016-05-02',
-        name: '寒雨',
-        address: '上海市普陀区金沙江路 1518 弄',
-        sex: '男'
-      }],
-      search: ''
+      tableData: [],
+      search: '',
+      show: true,
+      form: {
+        name: '',
+        bir: '',
+        sex: '男',
+        address: ''
+      }
     }
+  },
+  created () {
+    this.findAllTableData()
   },
   methods: {
     handleEdit (index, row) {
@@ -97,11 +114,43 @@ export default {
     },
     handleDelete (index, row) {
       console.log(index, row)
+    },
+    findAllTableData () {
+      this.$http.get('http://localhost:8084/user/findAll').then(res => {
+        this.tableData = res.data
+      })
+    },
+    onSubmit () {
+      this.$http.post('http://localhost:8084/user/save', this.form).then(res => {
+        if (res.data.status) {
+          this.$message({
+            message: '添加成功',
+            type: 'success'
+          })
+          this.form = {}
+          this.show = false
+          this.findAllTableData()
+        } else {
+          this.$message({
+            message: '添加失败',
+            type: 'error'
+          })
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-
+.transition-box {
+  margin-bottom: 10px;
+  width: 100%;
+  height: 400px;
+  border-radius: 4px;
+  color: #fff;
+  padding: 40px 20px;
+  box-sizing: border-box;
+  margin-right: 20px;
+}
 </style>
